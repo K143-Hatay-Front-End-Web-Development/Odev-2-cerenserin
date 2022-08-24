9321ba99528029cc11ea89c8205f738f43433a39
cerenserin <85118432+cerenserin@users.noreply.github.com>
2022-08-24 22:44:10 +0300

    up

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Game from "./components/Game/Game";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="text">GAME TIME</div>
+      <Game />
+    </div>
+  );
+}
+
+export default App;
